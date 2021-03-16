8ae01508faf76c416932f7e6b5d7786cb78a1f4b
Chanon latt <chanonlatt@gmail.com>
2021-03-16 20:24:23 +0700

    update splash screen

--- AllScreens/SplashScreen.dart
+++ AllScreens/SplashScreen.dart
@@ -1,13 +1,27 @@
 import 'package:flutter/material.dart';
 
-class SplashScreen extends StatefulWidget {
-  @override
-  _SplashScreenState createState() => _SplashScreenState();
+class SplashScreen extends StatelessWidget {
+  Color appColor = Color(0xFFF316B4);
+@override
+Widget build(BuildContext context) {
+  return Container(
+    color: appColor,
+    child: Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.asset("Assets/app_logo.png"),
+          SizedBox(height: 16),
+          Text("Neak Der Leng",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+                color: Colors.white
+            ),)
+        ],
+      ),
+    ),
+  );
 }
-
-class _SplashScreenState extends State<SplashScreen> {
-  @override
-  Widget build(BuildContext context) {
-    return Container();
-  }
 }
